df5b9dbf866e924dd7abeb07023063de722d29eb
Valentina Cuello <delmar_vale08@hotmail.com>
2021-04-16 23:20:48 +0000

    Me había faltado agregar el array action

--- js.js
+++ js.js
@@ -1,11 +1,14 @@
 var who = ['the dog','my granma','his turtle','my bird'];
+var action = ['ate','peed','crushed','broke'];
 var what = ['eat','pissed','crushed','broked'];
 var when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
+
 function excuseGenerator(){
     let whoIndex = Math.floor(Math.random() * who.length);
+    let actionIndex = Math.floor(Math.random());
     let whatIndex = Math.floor(Math.random() * what.length);
     let whenIndex = Math.floor(Math.random() * when.length);
 
-    document.getElementById("excuse").innerHTML = who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+    document.getElementById("excuse").innerHTML = who[whoIndex] + " " + action[actionIndex] + " " + what[whatIndex] + " " + when[whenIndex];
 };
